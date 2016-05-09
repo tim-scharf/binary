5952297b1d5ff5657813a3c6e9c906001ae34f8d
tim-scharf <timothy.scharf@uptake.com>
2016-05-09 12:59:32 -0500

    updates to package

--- R/classifier_loop.R
+++ R/classifier_loop.R
@@ -1,15 +1,29 @@
-classifier_loop <- function(Xtrain.rds,
-                            y.rds, 
-                            Xtest.rds, 
+#' Title
+#'
+#' @param Xtrain.feather
+#' @param y.feather
+#' @param Xtest.feather
+#' @param data_checks_func
+#' @param train_func
+#' @param iter
+#' @param pct_train
+#'
+#' @return
+#' @export
+#'
+#' @examples
+classifier_loop <- function(Xtrain.feather,
+                            y.feather,
+                            Xtest.feather,
                             data_checks_func,
                             train_func,
                             iter,
                             pct_train){
 
-#get data checks
-Xtrain <- readRDS(Xtrain.rds)
-Xtest  <- readRDS(Xtest.rds)
-y <- readRDS(y.rds)
+#get data and do checks
+Xtrain <- readRDS(Xtrain.feather)
+Xtest  <- readRDS(Xtest.feather)
+y <- readRDS(y.feather)
 data_checks_func(Xtrain,Xtest,y)
 
 #initialize output
--- R/init_OUPUT.R
+++ R/init_OUPUT.R
@@ -5,10 +5,9 @@
 #' @param n_train_full number of training examples
 #' @param n_test_full  number of test examples
 #' @param iter  how many iterations to run
-#' @param pct_train what % to train on (10-25% current use)
+#' @param pct_train what PCT 0,1 to train on)
 #'
-#' @return list PCV cv predictions, PT training predictions, IDX,
-#'     training indices, DATA meta data from training
+#' @return list PCV cv predictions
 #' @export
 #'
 #' @examples OUPUT <- init_OUPUT(1000,1000,100,.25)
